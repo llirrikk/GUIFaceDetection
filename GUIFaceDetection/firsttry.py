f5b35c1c847df5cc51d7226c0a736c9e021f3dee
Kirill <31863181+llirrikk@users.noreply.github.com>
2019-02-17 12:20:55 +0400

    Update firsttry.py
    сохранение обрезанных лиц

--- GUIFaceDetection/firsttry.py
+++ GUIFaceDetection/firsttry.py
@@ -1,11 +1,36 @@
 from tkinter import *
+from tkinter import messagebox as mb
 import cv2
 import sys
-from os import chdir
+from os import chdir, mkdir, path
 from random import choice, randint
 
 root = Tk()
 root.title("Главное окно")
+
+
+def check():
+    answer = mb.askyesno(title="Вопрос", message="Обрезать лица?")
+    if answer == True:
+        cascPath = "haarcascade.xml"
+
+        faceCascade = cv2.CascadeClassifier(cascPath)
+
+        image = cv2.imread(message.get())
+        filename = path.splitext(message.get())[0]
+        mkdir(filename + "_output")
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+       
+        i=0
+        for (x, y, w, h) in faces:
+            sub_img = image[y : y + h, x : x + w]
+            i=i+1
+            chdir(filename + "_output") 
+            cv2.imwrite(str(i) + ".jpg", sub_img)
+            chdir("../")
+            int(i)
+    else: exit()
 
 
 def about():
@@ -17,9 +42,8 @@ def about():
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    print("Found {0} faces.".format(len(faces)))
 
-    for x, y, w, h in faces:
+    for (x, y, w, h) in faces:
         sub_img = image[y: y + h, x: x + w]
         cv2.circle(
         img=image,
@@ -30,6 +54,8 @@ def about():
 
     cv2.imshow("Found {0} faces".format(len(faces)), image)
     cv2.waitKey(0)
+    check()
+
 
 message = StringVar()
 
